f436903f4bbe7601c4908bda55310e4c22e3bb31
M. de Verteuil <onceuponajooks@gmail.com>
2015-05-22 18:28:33 -0400

    Added sized number widget

---
+++ widgets/sizednumber/sizednumber.js
@@ -0,0 +1,13 @@
+/* global Dashing */
+
+Dashing.widgets.SizedNumber = function(dashboard) {
+    var self = this, widget;
+    self.__init__ = Dashing.utils.widgetInit(dashboard, 'sizednumber');
+    self.row = 2;
+    self.col = 2;
+    self.color = '#96bf48';
+    self.scope = { fontSize: '24px' };
+    self.getWidget = function () { return widget; };
+    self.getData = function () {};
+    self.interval = 1000;
+};
