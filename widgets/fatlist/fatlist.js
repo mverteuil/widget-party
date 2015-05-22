04d6c0c279a4cb36aad8161bf53ca92ae0fbda34
M. de Verteuil <onceuponajooks@gmail.com>
2015-05-22 15:34:14 -0400

    Rewrote fat list

--- widgets/fatlist/fatlist.js
+++ widgets/fatlist/fatlist.js
@@ -1,44 +1,13 @@
-/*global Dashboard, $*/
-Dashboard.widgets.FatList = function (dashboard) {
-    var self = this,
-        widget,
-        itemTemplate;
-    this.__init__ = function () {
-        var html = $('#templates').find('.widget-fatlist').clone();
-        widget = dashboard.grid.add_widget(
-            html,
-            self.col,
-            self.row);
-        itemTemplate = widget.find('.row').clone();
-    };
-    this.row = 1;
-    this.col = 2;
-    this.render = function () {
-        var fatList = self.getWidget();
-        var listItems = fatList.find('.fatlist-body');
-        
-        fatList.find('.title').text(self.data.title);
-        listItems.empty()
+/* global Dashing */
 
-        var listData = self.data.data;
-        if (listData) {
-            // Loop through list items
-            for (var listIndex = 0; listIndex < listData.length; listIndex++) {
-                var item = itemTemplate.clone();
-                var data = listData[listIndex];
-                // Loop through the rest of the properties and set the text
-                for (var fieldName in data) {
-                        item.find('.label').text(fieldName);
-                        item.find('.value').text(data[fieldName]);
-                }
-                listItems.append(item);
-            }
-        }
-    };
-    this.data = {};
-    this.getWidget = function () {
-        return widget;
-    };
-    this.getData = function () {};
-    this.interval = 10000;
+Dashing.widgets.FatList = function (dashboard) {
+    var self = this,
+        widget;
+    self.__init__ = Dashing.utils.widgetInit(dashboard, 'fatlist');
+    self.row = 2;
+    self.col = 2;
+    self.scope = {};
+    self.getWidget = function () { return widget; };
+    self.getData = function () {};
+    self.interval = 10000;
 };
