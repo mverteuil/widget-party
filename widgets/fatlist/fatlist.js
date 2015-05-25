d64b8104281776b28f3542a6bdc6699b45e7dbb6
M. de Verteuil <onceuponajooks@gmail.com>
2015-05-24 20:13:56 -0400

    Added a real fatlist

---
+++ widgets/fatlist/fatlist.js
@@ -0,0 +1,12 @@
+/* global Dashing */
+
+Dashing.widgets.FatList = function (dashboard) {
+    var self = this, widget;
+    self.__init__ = Dashing.utils.widgetInit(dashboard, 'fatlist');
+    self.row = 2;
+    self.col = 2;
+    self.scope = {};
+    self.getWidget = function () { return widget; };
+    self.getData = function () {};
+    self.interval = 10000;
+};
