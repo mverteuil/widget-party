0af3964096d5d42483c46d2c56cdd24a60dc7f9d
Liav Koren <liav@vmfarms.com>
2015-10-13 17:04:35 -0400

    Updates BuilderList template and rivets binder to resize font as list grows.

--- widgets/builderlist/builderlist.js
+++ widgets/builderlist/builderlist.js
@@ -1,5 +1,4 @@
 /* global Dashing */
-// foobar
 Dashing.widgets.BuilderList = function(dashboard) {
     var self = this, widget;
     self.__init__ = Dashing.utils.widgetInit(dashboard, 'builderlist');
@@ -12,7 +11,9 @@ Dashing.widgets.BuilderList = function(dashboard) {
     self.interval = 45000;
 };
 
+
 rivets.binders.color = function(el, value) {
+    // Update the builder color based on state.
     var state_colors = {
         failed: '#EB4720',
         progress: '#2A99FF',
@@ -21,4 +22,17 @@ rivets.binders.color = function(el, value) {
     };
 
     el.style.setProperty('color', state_colors[value]);
+};
+
+
+rivets.binders.fontsize = function(builderWidgetDiv, buildersList) {
+    var maxListItems = 13; // more than 13 items, start to shrink font
+    if (buildersList.length > maxListItems) {
+        var defaultFontSize = 25; //pixels
+        var decrementBy = 3; //px
+        var minFontSize = 7; //px. Completely illegible below this.
+        var decrementMultiplier = buildersList.length - maxListItems;
+        var newFontSize = Math.max(minFontSize, defaultFontSize - decrementMultiplier * decrementBy);
+        $(builderWidgetDiv).find('ul li').css({'font-size': newFontSize});
+    }
 };
