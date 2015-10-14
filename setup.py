c2bd843cd383cb55c3a74f48612a4969c893a42e
M. de Verteuil <onceuponajooks@gmail.com>
2015-10-14 14:46:05 -0400

    Create 2.3 release with recent PR merges

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -9,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='widget-party',
-    version='2.2.3',
+    version='2.3',
     packages=['widget_party'],
     install_requires=['django-dashing>=0.2.6', 'Django>=1.6', ],
     include_package_data=True,
